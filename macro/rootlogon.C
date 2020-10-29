816be2d7ddba80760ed75021ce12cb4c2c64c718
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2020-10-29 13:13:11 +0900

    rootlogon.C update

---
+++ macro/rootlogon.C
@@ -0,0 +1,8 @@
+{
+    std::cout << "========================================" <<std::endl<<std::endl;
+    std::cout << "ALPIDE Clustering Analysis Framework" <<std::endl;
+    std::cout <<std::endl<< "========================================" <<std::endl;
+    TString dirname = TString(gSystem->GetWorkingDirectory());
+    gInterpreter->AddIncludePath(TString::Format("%s/../include", dirname.Data()));
+    gSystem->AddIncludePath(TString::Format("%s/../include", dirname.Data()));
+}
