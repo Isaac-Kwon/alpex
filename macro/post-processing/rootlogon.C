2f3976ce8d814e2e1e7e461f3f17bb96b647784a
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2020-11-04 15:02:22 +0900

    rootlogon.C Update, macro folder structure update

--- macro/post-processing/rootlogon.C
+++ macro/post-processing/rootlogon.C
@@ -3,10 +3,10 @@
     std::cout << "ALPIDE Clustering Analysis Framework" <<std::endl;\
     std::cout <<std::endl<< "========================================" <<std::endl<<std::endl;
     std::cout << ">> WARNING" << std::endl;
-    std::cout << "Now you're in in directory \'AnalysisFramework/macro/\', the input-output filepath may be different with macro\'s expectation" <<std::endl;
+    std::cout << "Now you're in in directory \'AnalysisFramework/macro/postprocessing\', the input-output filepath may be different with macro\'s expectation" <<std::endl;
     std::cout << "If there's error with file I/O during running, run the macro in above directory (right below the \"AnalysisFramework\")" <<std::endl<<std::endl;
     
     TString dirname = TString(gSystem->GetWorkingDirectory());
-    gInterpreter->AddIncludePath(TString::Format("%s/../include", dirname.Data()));
-    gSystem->AddIncludePath(TString::Format("%s/../include", dirname.Data()));
+    gInterpreter->AddIncludePath(TString::Format("%s/../../include", dirname.Data()));
+    gSystem->AddIncludePath(TString::Format("%s/../../include", dirname.Data()));
 }
